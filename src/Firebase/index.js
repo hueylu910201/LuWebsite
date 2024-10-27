c40e875eee4a2ed1dd778eded5f3e1dbe881656b
呂修逸 <hueylu910201@gmail.com>
2024-10-27 23:27:06 +0800

    Update firebase index.js

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -7,20 +7,4 @@ import { getDatabase, ref, push } from 'firebase/database'; // 使用 Realtime D
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCjVBzkeIN62GoKi4LX4WTvVMjwzoJQxk4",
-  authDomain: "personalweb-d08e6.firebaseapp.com",
-  projectId: "personalweb-d08e6",
-  storageBucket: "personalweb-d08e6.appspot.com",
-  messagingSenderId: "96082816081",
-  appId: "1:96082816081:web:9260f2da2155c24f6c9c70",
-  measurementId: "G-BHW490C3F8"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const db = getFirestore(app);  // 初始化 Firestore
-
-export { database, ref, push };
-export { db, collection, addDoc };
