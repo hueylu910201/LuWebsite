1e5644dab720962eb22126cd60fc31fddc705527
hueylu910201 <hueylu910201@gmail.com>
2024-12-21 18:55:30 +0800

    fix firebase bug

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -7,4 +7,20 @@ import { getDatabase, ref, push } from 'firebase/database'; // 使用 Realtime D
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: "G-BHW490C3F8"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const db = getFirestore(app);  // 初始化 Firestore
+
+export { database, ref, push };
+export { db, collection, addDoc };
